2e75571dbe32454c0968eb6a2143ce501de010cb
Nguyễn Kim Vương Ý <134944912+vuongynk@users.noreply.github.com>
2025-05-25 18:04:50 +0700

    Add .next/static/chunks/src_app_admin_layout_tsx_e8bc069c._.js 1748171089888

---
+++ .next/static/chunks/src_app_admin_layout_tsx_e8bc069c._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_3780d5b2._.js"
+],
+    source: "dynamic"
+});
